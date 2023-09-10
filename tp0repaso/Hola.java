ed07586defc2b135f335fc544f559eab09adb839
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2023-09-10 23:36:29 +0000

    ok

--- tp0repaso/Hola.java
+++ tp0repaso/Hola.java
@@ -4,5 +4,12 @@ public class Hola{
     System.out.println("Bienvenidos a POO Practica");
     for (int i=0;i<10;i++)
       System.out.println(i);
+   
+     System.out.println( Hola.prueba());
+    
+  }
+  public static String prueba()
+  {
+    return true ? "si" : "No";
   }
 }
